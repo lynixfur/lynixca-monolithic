ffee7f9b5b4758f9da5f101131fca3f8abe250d0
Lynix <81735347+cyberlynix@users.noreply.github.com>
2024-02-20 21:33:35 -0400

    Fix some errors while compilation

--- src/app/fursona/page.tsx
+++ src/app/fursona/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
                         <img src="/lynix.png" className="w-20 mb-1.5" alt="lynix" />
                         <h1 className="text-5xl font-semibold">My Fursona - Lynix</h1>
                     </div>
-                    <p className="text-neutral-400 mt-5">Here's Lynix! My fursona and mascot of my cybersecurity identity. <br/><br/>
+                    <p className="text-neutral-400 mt-5">Here&apos;s Lynix! My fursona and mascot of my cybersecurity identity. <br/><br/>
                                 Lynix is a unique character known as a Freakhound, a canine hybrid with
                                 reptilian traits on its tail. Hailing from deep space, Lynix possesses
                                 advanced technological knowledge and aids in protecting Earth against cyber
@@ -29,13 +29,13 @@ export default function Home() {
                     <br/>
 
                     <p className="mt-3 text-base sm:text-md text-neutral-400">
-                        <span className="text-cyan-500 text-md font-bold">What's a fursona?</span> A fursona
+                        <span className="text-cyan-500 text-md font-bold">What&apos;s a fursona?</span> A fursona
                         is a customized animal character representing someone in the furry fandom,
                         ranging from anthropomorphic personas to roleplay characters or digital
                         mascots.
                     </p>
                     <p className="text-base sm:text-md mt-2 text-neutral-400">
-                        <span className="text-cyan-500 text-md  font-bold">What's a furry?</span> A furry is
+                        <span className="text-cyan-500 text-md  font-bold">What&apos;s a furry?</span> A furry is
                         someone who is a fan or enthusiast of characters that depict animals with
                         human characteristics, such as talking, walking on two legs, and displaying
                         human-like emotions and behaviors.
@@ -74,12 +74,12 @@ export default function Home() {
                         <br />
                         Here are some pictures of my fursuit being made and some pictures of myself wearing it at conventions or events. <br/><br/>
 
-                        My fursuit development has started with Goatfangs making me some paws for an upcoming convention in 2024 which is named Furnal Equinox. I'm excited to see the final product and wear it at the convention. As the project evolves more accessories and parts will be added to the suit. Stay tuned for more updates!
+                        My fursuit development has started with Goatfangs making me some paws for an upcoming convention in 2024 which is named Furnal Equinox. I&apos;m excited to see the final product and wear it at the convention. As the project evolves more accessories and parts will be added to the suit. Stay tuned for more updates!
                     </p>
 
                     <div className="bg-neutral-800 p-5 rounded-md mt-5 flex flex-col justify-center items-center">
                         <img src="/fursona/fursuit/PAW_WIP2.png" alt="lynix" className="rounded mt-5 max-w-full md:max-w-[350px] mb-2" />
-                        <p className="mt-2 text-sm">This is a WIP Photo of Lynix's paws being made for FE 2024</p>
+                        <p className="mt-2 text-sm">This is a WIP Photo of Lynix&apos;s paws being made for FE 2024</p>
                         <p className="font-bold text-cyan-600">Goatfangs - 20 Feb 2024</p>
                     </div>
 
@@ -99,7 +99,7 @@ export default function Home() {
                         
                         <div className="bg-neutral-800 p-5 rounded-md mt-5 flex flex-col justify-center items-center">
                             <img src="/fursona/gallery/pfp.png" alt="lynix" className="rounded mt-5 max-w-[150px] mb-2" />
-                            <p className="mt-2 text-sm">Lynix's pfp</p>
+                            <p className="mt-2 text-sm">Lynix&apos;s pfp</p>
                             <p className="font-bold text-cyan-600">Mulliganos</p>
                         </div>
 
--- src/components/modals/FE.tsx
+++ src/components/modals/FE.tsx
@@ -21,22 +21,22 @@ export default function FE() {
                 <div className="flex items-center justify-between p-4 md:p-5 rounded-t">
                 <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
                     <img src="/lynix.png" alt="lynix" className="w-14 h-14 rounded-full mb-2" />
-                    Hewwo! I'm Lynix! Thanks for meeting me at <br/><strong className="text-cyan-600">Furnal Equinox 2024</strong>
+                    Hewwo! I&apos;m Lynix! Thanks for meeting me at <br/><strong className="text-cyan-600">Furnal Equinox 2024</strong>
                 </h3>
                 </div>
                 {/* Modal body */}
                 <div className="p-4 md:p-5 space-y-4">
                 <p className="text-base leading-relaxed text-gray-500 dark:text-neutral-300">
                     Loving Furnal Equinox so far? Thanks for hanging out and chatting earlier. 😊<br/><br/>
-                    I'm a Canadian fur from Moncton, NB with a passion for cybersecurity and all things techy hence my fancy badges and NFC paws.
-                    <br/><br/><i>Awoooooooo! And don't forget that badge boop!</i>
+                    I&apos;m a Canadian fur from Moncton, NB with a passion for cybersecurity and all things techy hence my fancy badges and NFC paws.
+                    <br/><br/><i>Awoooooooo! And don&apos;t forget that badge boop!</i>
                 </p>
                 <p style={{"fontSize": "12px", "marginTop": "2px"}}>Badge Scanned: {nfc_tag}</p>
                 </div>
                 {/* Modal footer */}
                 <div className="flex items-center p-4 md:p-5 border-gray-200 rounded-b space-x-2">
-                <button data-modal-hide="default-modal" type="button" className="text-white bg-neutral-600 hover:bg-neutral-700 transition-colors focus:ring-4 focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center"><i className="fa-brands fa-telegram"></i> Telegram</button>
-                <button data-modal-hide="default-modal" type="button" className="text-white bg-cyan-700 hover:bg-cyan-800 transition-colors focus:ring-4 focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center"><i className="fa-solid fa-paw"></i> Awooo!</button>
+                <a href="https://t.me/lynixfur" className="text-white bg-neutral-600 hover:bg-neutral-700 transition-colors focus:ring-4 focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center"><i className="fa-brands fa-telegram"></i> Telegram</a>
+                <a href="https://lynix.ca" className="text-white bg-cyan-700 hover:bg-cyan-800 transition-colors focus:ring-4 focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center"><i className="fa-solid fa-paw"></i> Awooo!</a>
                 </div>
             </div>
             </div>
